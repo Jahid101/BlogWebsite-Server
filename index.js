b18108d6d37ece1e30d960d0e4a5de62ba605775
Jahid101 <jahidhasananik.official@gmail.com>
2021-06-26 21:00:45 +0600

    almost done

--- index.js
+++ index.js
@@ -24,57 +24,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const serviceCollection = client.db("fixmypc").collection("services");
-  const feedbackCollection = client.db("fixmypc").collection("feedbacks");
-  const adminCollection = client.db("fixmypc").collection("admins");
-  const orderCollection = client.db("fixmypc").collection("orders");
+  const blogCollection = client.db("Blog").collection("blogs");
+  const adminCollection = client.db("Blog").collection("admins");
+  const feedbackCollection = client.db("Blog").collection("feedbacks");
 
 
-  app.post('/addService', (req, res) => {
-    const newService = req.body;
-    serviceCollection.insertOne(newService)
+  app.post('/addBlog', (req, res) => {
+    const newBlog = req.body;
+    blogCollection.insertOne(newBlog)
       .then(result => {
         res.send(result.insertedCount > 0)
       })
   })
 
 
-  app.post('/updatedOrder', (req, res) => {
-    const updatedOrder = req.body;
-    updatedOrderCollection.insertOne(updatedOrder)
-      .then(result => {
-        res.send(result.insertedCount > 0)
+  app.get('/blog', (req, res) => {
+    blogCollection.find()
+      .toArray((err, blogs) => {
+        res.send(blogs);
       })
   })
 
 
-  app.get('/service', (req, res) => {
-    serviceCollection.find()
-      .toArray((err, services) => {
-        res.send(services);
-      })
-  })
-
-
-  app.get('/serviceBooking/:id', (req, res) => {
+  app.get('/showBlog/:id', (req, res) => {
     const id = ObjectID(req.params.id)
-    serviceCollection.find({ _id: id })
-      .toArray((err, services) => {
-        res.send(services[0]);
+    blogCollection.find({ _id: id })
+      .toArray((err, blogs) => {
+        res.send(blogs[0]);
       })
   })
 
 
-  app.patch('/updateOrderList/:id', (req, res) => {
-    const id = ObjectID(req.params.id)
-        orderCollection.updateOne({ _id: id },
-            {
-                $set: { status: req.body.status }
-            })
-            .then(result => {
-                res.send(result.modifiedCount > 0 )
-            })
-    })
+  app.post('/checkAdmin', (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+        .toArray((err, admins) => {
+            res.send(admins.length > 0);
+        })
+})
 
 
   app.post('/addFeedback', (req, res) => {
@@ -94,59 +81,14 @@ client.connect(err => {
   })
 
 
-  app.post('/addAdmin', (req, res) => {
-    const newAdmin = req.body;
-    adminCollection.insertOne(newAdmin)
-      .then(result => {
-        res.send(result.insertedCount > 0)
-      })
-  })
-
-
-
-  app.post('/checkAdmin', (req, res) => {
-    const email = req.body.email;
-    adminCollection.find({ email: email })
-        .toArray((err, admins) => {
-            res.send(admins.length > 0);
-        })
-})
-
-
-
-  app.post('/addOrder', (req, res) => {
-    const newOrder = req.body;
-    orderCollection.insertOne(newOrder)
-      .then(result => {
-        res.send(result.insertedCount > 0)
-      })
-  })
-
-
-
-  app.delete('/deleteService/:id', (req, res) => {
+  app.delete('/deleteBlog/:id', (req, res) => {
     const id = ObjectID(req.params.id)
-    serviceCollection.deleteOne({ _id: id })
+    blogCollection.deleteOne({ _id: id })
       .then(result => {
         res.send(result.deletedCount > 0)
       })
   })
 
 
-app.get('/serviceList', (req, res) => {
-    orderCollection.find({ email: req.query.email })
-      .toArray((err, orders) => {
-        res.send(orders);
-      })
-  })
 
-
-app.get('/orderList', (req, res) => {
-    orderCollection.find()
-      .toArray((err, orders) => {
-        res.send(orders);
-      })
-  })
-
-  
 });
